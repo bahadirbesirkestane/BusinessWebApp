c0f715dbd8450b65be31fd27dd219d014bcdf787
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-19 16:07:30 +0300

    Controllerlar ve deneme sayfası eklendi

---
+++ Business/Business/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Business.BusinessLayer.Abstract;
+using Business.BusinessLayer.Concrete;
+using Business.DataAccess.Abstact;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Business.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _categoryService.GetList();
+
+            return View();
+        }
+    }
+}
