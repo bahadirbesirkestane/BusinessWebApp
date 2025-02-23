9eb1ee3ae2c51062173bfc89987c056bfc0077d7
isim soyisim <bahadir_kestane@hotmail.com>
2025-02-23 15:10:43 +0300

    view components eklendi

---
+++ Business/Business/ViewComponents/Admin/AdminMessageNotification.cs
@@ -0,0 +1,21 @@
+﻿using Business.BusinessLayer.Abstract;
+using Business.BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Business.ViewComponents.Admin
+{
+    public class AdminMessageNotification : ViewComponent
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminMessageNotification(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Business/Business/ViewComponents/Admin/AdminNotification.cs
@@ -0,0 +1,20 @@
+﻿using Business.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Business.ViewComponents.Admin
+{
+    public class AdminNotification : ViewComponent
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminNotification(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
